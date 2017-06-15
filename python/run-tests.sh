e8fdabddcd09ee45322ac9526ddd5819c7190519
Philip Yang <philip.yang@databricks.com>
2017-06-14 17:03:17 -0700

    test capture logs

--- python/run-tests.sh
+++ python/run-tests.sh
@@ -103,7 +103,7 @@ do
   # The grep below is a horrible hack for spark 1.x: we manually remove some log lines to stay below the 4MB log limit on Travis.
   $PYSPARK_DRIVER_PYTHON \
       -m "nose" \
-      --nocapture --nologcapture \
+      --nologcapture \
       -v --exe "$noseOptions" \
       2>&1 | grep -vE "INFO (ParquetOutputFormat|SparkContext|ContextCleaner|ShuffleBlockFetcherIterator|MapOutputTrackerMaster|TaskSetManager|Executor|MemoryStore|CacheManager|BlockManager|DAGScheduler|PythonRDD|TaskSchedulerImpl|ZippedPartitionsRDD2)";
 
