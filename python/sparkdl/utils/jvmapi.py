3a880efdf32c974fd36c1be0444ffbcea726e938
Philip Yang <philip.yang@databricks.com>
2017-06-20 13:56:08 -0700

    [upstream-prep] Keras image model UDF (#6)
    * Building UDFs from TensorFlow Graphs
    
    We built a thin wrapper on top of TensorFlow's Session
    to help us creating functions (and specifically, UDFs)
    and composing subgraph (either Keras or TensorFlow).
    
    * Helpers from JVM's side
    
    * GraphFunction and GraphBuilderSession usage
    
    * (graph builder) unit tests
    
    * not exporting developer APIs
    
    * graph factory unit tests
    
    * update graph factory tests
    
    * PR comments: migrating utils to a separate module
    
    1. Move JVMAPI to a submodule as it is more pythonic
    2. Some unit tests on JVMAPI
    
    * PR comments: graph utils refactoring
    
    1. More meaningful names: `GraphBuilderSession` => `IsolatedSession`
    2. Tensorflow graph utils resides in a single module
       (as opposed to a static class)
    3. `GraphFunction` static methods
    
    * PR comments
    
    1. Renaming submodules
    2. Using absolute import as per PEP 8
    
    * PR comments:
    
    1. Removed `joblib` dependency
    2. Keras model import minor refactor
    
    * PR comments
    
    1. fix requirements.txt
    2. removed pathlib in favor of os.path
    3. removed identity piece
    4. using JSON + binary file for graph function storage
    
    * utils update
    
    * temporary travis
    
    * utils fix
    
    * test capture logs
    
    * PR comments
    
    * try catch tmpdir
    
    * PR comments
    
    1. docs
    2. graph type check in graph.utils
    
    * sbt build spill out classpath for testing
    
    * Keras UDF
    
    * fix tests
    
    * Revert "Keras UDF"
    
    This reverts commit da9d79216cdf8a6c8d714f764288fd0973df69ca.
    
    * keras UDF
    
    * minor cleanup
    
    * Building UDFs from TensorFlow Graphs
    
    We built a thin wrapper on top of TensorFlow's Session
    to help us creating functions (and specifically, UDFs)
    and composing subgraph (either Keras or TensorFlow).
    
    * Helpers from JVM's side
    
    * GraphFunction and GraphBuilderSession usage
    
    * (graph builder) unit tests
    
    * not exporting developer APIs
    
    * graph factory unit tests
    
    * update graph factory tests
    
    * PR comments: migrating utils to a separate module
    
    1. Move JVMAPI to a submodule as it is more pythonic
    2. Some unit tests on JVMAPI
    
    * PR comments: graph utils refactoring
    
    1. More meaningful names: `GraphBuilderSession` => `IsolatedSession`
    2. Tensorflow graph utils resides in a single module
       (as opposed to a static class)
    3. `GraphFunction` static methods
    
    * PR comments
    
    1. Renaming submodules
    2. Using absolute import as per PEP 8
    
    * PR comments:
    
    1. Removed `joblib` dependency
    2. Keras model import minor refactor
    
    * PR comments
    
    1. fix requirements.txt
    2. removed pathlib in favor of os.path
    3. removed identity piece
    4. using JSON + binary file for graph function storage
    
    * utils update
    
    * temporary travis
    
    * utils fix
    
    * PR comments
    
    * try catch tmpdir
    
    * PR comments
    
    1. docs
    2. graph type check in graph.utils
    
    * sbt build spill out classpath for testing
    
    * Revert "sbt build spill out classpath for testing"
    
    This reverts commit 962e23aa4876cd26558c99e37c0970792e09c74a.
    
    * addressing comments
    
    fix tests for name changes
    
    * further PR comments
    
    * remove dump
    
    * renaming tests
    
    * current status
    
    * testing tooling
    
    * update keras image udf tests
    
    * removing serialization
    
    * removing visualization

--- python/sparkdl/utils/jvmapi.py
+++ python/sparkdl/utils/jvmapi.py
@@ -59,6 +59,22 @@ def default():
     """ Default JVM Python Interface class """
     return forClass(javaClassName=PYTHON_INTERFACE_CLASSNAME)
 
-def list_to_vector_udf(col):
+def listToMLlibVectorUDF(col):
     """ Map struct column from list to MLlib vector """
     return Column(default().listToMLlibVectorUDF(col._jc))  # pylint: disable=W0212
+
+def registerPipeline(name, ordered_udf_names):
+    """ 
+    Given a sequence of @ordered_udf_names f1, f2, ..., fn
+    Create a pipelined UDF as fn(...f2(f1()))
+    """
+    assert len(ordered_udf_names) > 1, \
+        "must provide more than one ordered udf names"
+    return default().registerPipeline(name, ordered_udf_names)
+
+def registerUDF(name, function_body, schema):
+    """ 
+    Given a sequence of @ordered_udf_names f1, f2, ..., fn
+    Create a pipelined UDF as fn(...f2(f1()))
+    """
+    return _curr_sql_ctx().registerFunction(name, function_body, schema)
