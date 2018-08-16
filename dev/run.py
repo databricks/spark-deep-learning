6be7772407feca0d9bc69e1ce97c8a034310e0e7
Yogesh Garg <1059168+yogeshg@users.noreply.github.com>
2018-08-16 12:19:45 -0700

    bump spark version (#150)
    * bump spark version
    
    * update default value of SPARK_HOME

--- dev/run.py
+++ dev/run.py
@@ -125,8 +125,8 @@ def print_if(cond, *args):
 
 def _get_required_env(default=False, interactive=False, override=False, verbose=False):
     default_env = {'PYSPARK_PYTHON': 'python',
-                   'SPARK_VERSION': '2.3.0',
-                   'SPARK_HOME': os.path.join(HOME, 'bin/spark-2.3.0-bin-hadoop2.7/'),
+                   'SPARK_VERSION': '2.3.1',
+                   'SPARK_HOME': os.path.join(HOME, 'bin/spark-2.3.1-bin-hadoop2.7/'),
                    'SCALA_VERSION': '2.11.8'}
 
     if override and not (interactive or default):
