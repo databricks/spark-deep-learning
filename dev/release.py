86f8a16ffb6028a48ce721ea314801b0dea07f7f
Bago Amirbekian <bago@databricks.com>
2018-11-29 22:33:34 -0800

    Some chages based on PR.

--- dev/release.py
+++ dev/release.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import click
+from datetime import datetime
 from six.moves import input
 from subprocess import check_call, check_output
 import sys
 
 DATABRICKS_REMOTE = "git@github.com:databricks/spark-deep-learning.git"
-PUBLISH_MODES = {"local": "publishLocal", "m2": "publishM2", "spark-package-local": "spDist",
-                 "spark-package-publish": "sbPublish"}
+PUBLISH_MODES = {"local": "publishLocal", "m2": "publishM2", "spark-package-publish": "spPublish"}
 
-WORKING_BRANCH = "WORKING_BRANCH_RELEASE_%s"
-GH_PAGES_BRANCH = "gh-pages-%s"
+WORKING_BRANCH = "WORKING_BRANCH_RELEASE_%s_@%s-%s-%s"
+WORKING_DOCS_BRANCH = "WORKING_BRANCH_DOCS_%s_@%s-%s-%s"
+RELEASE_TAG = "v%s"
+DOCS_TAG = "gh-pages-v%s"
+
 
 def prominentPrint(x):
     x = str(x)
@@ -38,6 +41,7 @@ def verify(prompt, interactive):
 def main(release_version, next_version, publish_to, no_prompt, git_remote):
     interactive = not no_prompt
 
+    time = datetime.now()
     if publish_to not in PUBLISH_MODES:
         modes = list(PUBLISH_MODES.keys())
         print("Unknown publish target, --publish-to should be one of: %s." % modes)
@@ -52,29 +56,32 @@ def main(release_version, next_version, publish_to, no_prompt, git_remote):
         sys.exit(1)
 
     current_branch = check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip()
-    if current_branch != "master":
+    if current_branch != b"master":
         if not verify("You're not on the master branch do you want to continue? (y/n)",
                       interactive):
             sys.exit(1)
 
     uncommitted_changes = check_output(["git", "diff", "--stat"])
-    if uncommitted_changes != "":
+    if uncommitted_changes != b"":
         print(uncommitted_changes)
         print("There seem to be uncommitted changes on your current branch. Please commit or "
               "stash them and try again.")
         sys.exit(1)
 
-    working_branch = WORKING_BRANCH % release_version
-    gh_pages_branch = GH_PAGES_BRANCH % release_version
-    local_branches = check_output(["git", "branch"])
-    target_branches = [gh_pages_branch, working_branch, "gh-pages"]
-    conflict_branches = list(filter(lambda a: a in local_branches, target_branches))
-    if conflict_branches:
+    working_branch = WORKING_BRANCH % (release_version, time.hour, time.minute, time.second)
+    gh_pages_branch = WORKING_DOCS_BRANCH % (release_version, time.hour, time.minute, time.second)
 
-        msg = ("This script needs to replace the following branches:\n"
+    release_tag = RELEASE_TAG % release_version
+    docs_tag = DOCS_TAG % release_version
+    target_tags = [release_tag, docs_tag]
+
+    existing_tags = check_output(["git", "tag"]).decode().split()
+    conflict_tags = list(filter(lambda a: a in existing_tags, target_tags))
+    if conflict_tags:
+        msg = ("The following tags already exist:\n"
                "    %s\n"
                "Please delete them and try.")
-        msg = msg % "\n    ".join(conflict_branches)
+        msg = msg % "\n    ".join(conflict_tags)
         prominentPrint(msg)
         sys.exit(1)
 
@@ -86,7 +93,6 @@ def main(release_version, next_version, publish_to, no_prompt, git_remote):
     check_call(["./build/sbt", update_version])
 
     prominentPrint("Building and testing with sbt.")
-    release_tag = "v%s" % release_version
     check_call(["git", "checkout", release_tag])
 
     publish_target = PUBLISH_MODES[publish_to]
@@ -115,13 +121,15 @@ def main(release_version, next_version, publish_to, no_prompt, git_remote):
     commit_message = "Build docs for release %s." % release_version
     check_call(["git", "add", "-f", "docs/_site"])
     check_call(["git", "commit", "-m", commit_message])
-    msg = "Would you like to push docs branch to remote, %s, and update gh-pages branch? (y/n)"
-    msg %= git_remote
+    check_call(["git", "tag", docs_tag])
+    msg = "Would you like to push %s to remote, %s, and update gh-pages branch? (y/n)"
+    msg %= (docs_tag, git_remote)
     if verify(msg, interactive):
-        check_call(["git", "push", git_remote, gh_pages_branch])
+        check_call(["git", "push", git_remote, docs_tag])
         check_call(["git", "push", "-f", git_remote, gh_pages_branch+":gh-pages"])
-        check_call(["git", "checkout", current_branch])
-        check_call(["git", "branch", "-D", gh_pages_branch])
+
+    check_call(["git", "checkout", current_branch])
+    check_call(["git", "branch", "-D", gh_pages_branch])
 
 
 if __name__ == "__main__":
